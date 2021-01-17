759953d0dfc81ba772049736fcb58a0c51019215
jsgarzi <jsgarzi@fortitudedevelopment.com>
2021-01-17 18:14:29 -0500

    Setting up new repo to link w/ heroku

---
+++ public/js/blogCreate.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+  var titleInput = $("#titleInput");
+  var bodyInput = $("#textInput");
+
+  var userId;
+  var postId;
+  var editPost = false;
+
+  function getUser() {
+    $.get("/api/userdata").then(function (data) {
+      userId = data.id;
+    }).then(function () {
+      loadBlog();
+    });
+  }
+  getUser();
+  // When the post button is clicked, validate that the title and body are not blank
+  $("#postBtn").on("click", function (event) {
+
+    if (editPost ===true) {
+      editPost = false;
+      // console.log(titleInput.val().trim());
+      // console.log(bodyInput.val().trim());
+
+
+      $.ajax({
+        method: "PUT",
+        url: "/api/posts/" + postId,
+        data: {
+          title: titleInput.val().trim(),
+          body: bodyInput.val().trim()
+        }
+      }).then(function () {
+        window.location.replace("/userblog");
+      });
+    } else {
+      event.preventDefault();
+      // get userid from localstorage
+
+      var blogData = {
+        title: titleInput.val().trim(),
+        body: bodyInput.val().trim(),
+        AuthorId: "" + userId
+      };
+      if (!blogData.body || !blogData.title) {
+        return;
+      }
+
+      $.post("/api/createblog", blogData, function () {
+        // bodyInput.val("");
+        // titleInput.val("");
+        window.location.replace("/viewblogs");
+      });
+    }
+  });
+
+  function loadBlog() {
+    postId = localStorage.getItem("postId");
+    localStorage.setItem("postId", null);
+
+    if (postId !== "null") {
+      editPost = true;
+
+      $.get("/api/specific-post/" + postId, function (data) {
+        $("#titleInput").val(data.title);
+        $("#textInput").val(data.body);
+      });
+    }
+  }
+
+
+});
